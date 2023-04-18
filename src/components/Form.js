1f9e52445368ac526ce627b99ab58d8ecb1fdc6a
cliff <cliffmudigo5@gmail.com>
2023-04-18 13:07:07 +0300

    completed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function Form(props) {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
+  const [error, setError] = useState("");
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,11 +13,31 @@ function Form(props) {
     setLastName(event.target.value);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    // Form validation logic
+    if (firstName.trim() === "" || lastName.trim() === "") {
+      setError("Please enter first name and last name.");
+      return;
+    }
+
+    const formData = {
+      firstName: firstName,
+      lastName: lastName,
+    };
+    props.sendFormDataSomewhere(formData);
+    setFirstName("");
+    setLastName("");
+    setError("");
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
+      {error && <p>{error}</p>}
     </form>
   );
 }
